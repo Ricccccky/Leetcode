263c4e8974a6bc8f5d143a11fe1d3c9cd4378bf1
Ricky <wbc0203@163.com>
2020-03-08 03:33:34 -0400

    two-pointer

--- 27.remove-element.py
+++ 27.remove-element.py
@@ -7,6 +7,12 @@
 # @lc code=start
 class Solution:
     def removeElement(self, nums: List[int], val: int) -> int:
-        
+        NumLen = len(nums)
+        j = 0
+        for i in range(NumLen):
+            if nums[i] != val:
+                nums[j] = nums[i]
+                j += 1
+        return j
 # @lc code=end
 

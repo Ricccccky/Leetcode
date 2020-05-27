2ac056f038eba66533049cbf209513fc5a21f636
Ricky <wbc0203@163.com>
2020-05-27 03:22:40 -0400

    5.26

---
+++ 112.path-sum.java
@@ -0,0 +1,44 @@
+import java.util.LinkedList;
+import java.util.Queue;
+
+/*
+ * @lc app=leetcode id=112 lang=java
+ *
+ * [112] Path Sum
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public boolean hasPathSum(TreeNode root, int sum) {
+        if (root == null) {
+            return false;
+        }
+
+        int res = sum - root.val;
+        if (root.left == null && root.right == null) {
+            if (res == 0) {
+                return true;
+            } else {
+                return false;
+            }
+        }
+        return hasPathSum(root.left, res) || hasPathSum(root.right, res);
+    }
+}
+// @lc code=end
+
---
+++ 236.lowest-common-ancestor-of-a-binary-tree.java
@@ -0,0 +1,42 @@
+/*
+ * @lc app=leetcode id=236 lang=java
+ *
+ * [236] Lowest Common Ancestor of a Binary Tree
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        if (root == null) {
+            return null;
+        } else if (root.val == p.val || root.val == q.val) {
+            return root;
+        }
+
+        TreeNode left = lowestCommonAncestor(root.left, p, q);
+        TreeNode right = lowestCommonAncestor(root.right, p, q);
+        if (left != null && right != null) {
+            return root;
+        } else {
+            if (left != null) {
+                return left;
+            } else if (right != null) {
+                return right;
+            } else {
+                return null;
+            }
+        }
+
+    }
+}
+// @lc code=end
+

3f2f4a5902e8f3c987f8545c2f24af29aa604466
Ricky <wbc0203@163.com>
2021-01-29 15:51:44 -0500

    1.29 AM

---
+++ Java/1296.divide-array-in-sets-of-k-consecutive-numbers.java
@@ -0,0 +1,34 @@
+import java.util.Arrays;
+import java.util.Map;
+import java.util.TreeMap;
+
+/*
+ * @lc app=leetcode id=1296 lang=java
+ *
+ * [1296] Divide Array in Sets of K Consecutive Numbers
+ */
+
+// @lc code=start
+class Solution {
+    public boolean isPossibleDivide(int[] nums, int k) {
+        Map<Integer, Integer> map = new TreeMap<>();
+        for (int num : nums) {
+            map.put(num, map.getOrDefault(num, 0) + 1);
+        }
+        for (int num : map.keySet()) {
+            if (map.get(num) > 0) {
+                int temp = map.get(num);
+                for (int i = 0; i < k; i++) {
+                    if (map.getOrDefault(num + i, 0) < temp) {
+                        return false;
+                    }
+                    map.put(num + i, map.get(num + i) - temp);
+                }
+            }
+        }
+
+        return true;
+    }
+}
+// @lc code=end
+
---
+++ Java/974.subarray-sums-divisible-by-k.java
@@ -0,0 +1,30 @@
+import java.util.HashMap;
+import java.util.Map;
+
+/*
+ * @lc app=leetcode id=974 lang=java
+ *
+ * [974] Subarray Sums Divisible by K
+ */
+
+// @lc code=start
+class Solution {
+    public int subarraysDivByK(int[] A, int K) {
+        int res = 0, sum = 0;
+        Map<Integer, Integer> map = new HashMap<>();
+        map.put(0, 1);
+        for (int a : A) {
+            sum += a;
+            int mod = sum % K;
+            if (mod < 0) {
+                mod += K;
+            }
+            res += map.getOrDefault(mod, 0);
+            map.put(mod, map.getOrDefault(mod, 0) + 1);
+        }
+
+        return res;
+    }
+}
+// @lc code=end
+

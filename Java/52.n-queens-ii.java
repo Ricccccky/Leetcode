ea3776be0251df8cbfb81551c7edaa9ef9ee4fad
Ricccccky <wbc0203@163.com>
2021-02-18 19:29:33 -0500

    test git

---
+++ Java/52.n-queens-ii.java
@@ -0,0 +1,14 @@
+/*
+ * @lc app=leetcode id=52 lang=java
+ *
+ * [52] N-Queens II
+ */
+
+// @lc code=start
+class Solution {
+    public int totalNQueens(int n) {
+        
+    }
+}
+// @lc code=end
+

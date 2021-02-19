da155eb6c669e083f71b72b93e62e7694e267b39
Ricccccky <wbc0203@163.com>
2021-02-18 19:19:34 -0500

    Update 1.two-sum.java

--- Java/1.two-sum.java
+++ Java/1.two-sum.java
@@ -10,14 +10,14 @@ import java.util.Map;
 // @lc code=start
 class Solution {
     public int[] twoSum(int[] nums, int target) {
-        Map<Integer, Integer> temp = new HashMap<>();
+        Map<Integer, Integer> map = new HashMap<>();
         int[] result = new int[2];
         for (int i = 0; i < nums.length; i++) {
-            if (temp.containsKey(target - nums[i])) {
-                result[0] = temp.get(target - nums[i]);
+            if (map.containsKey(target - nums[i])) {
+                result[0] = map.get(target - nums[i]);
                 result[1] = i;
             }
-            temp.put(nums[i], i);
+            map.put(nums[i], i);
         }
         return result;
     }
